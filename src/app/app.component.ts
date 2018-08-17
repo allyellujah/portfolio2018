45330aba3af2a457cc221b3990c5422a8ba64e49
Allye Vice <allye@filamentlabs.com>
2018-08-17 16:42:22 -0400

    contentful API fetching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+declare var jquery:any;
+declare var $ :any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  apiUrl = '';
+  data: object;
+
+  constructor(private http: HttpClient) {
+    this.getData();
+  }
+
+  getData() {
+    return this.http.get(this.apiUrl)
+    .subscribe(data => {
+      // console.log(data);
+      this.data = data["items"];
+    });
+  }
+
+  ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    
+  }
+
 }
